e22f606e0be132e5791222544992887a3af0a01a
gomezmadrid118-sketch <gomezmadrid118@gmail.com>
2026-08-23 22:31:14 -0500

    se coloca el codigo principal del jsx de Tarjeta, ahora toca darle estilos

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -3,7 +3,17 @@ import { usuarios } from '../data/usuarios'
 const Tarjeta = () => {
   return (
     <>
-    <p>Trabajo en progreso, favor vuelva mas tarde</p>
+      {usuarios.map((usuario) => 
+        (<div key={usuario.id} className="tarjeta_usuario">
+          <h3>{usuario.nombre}</h3>
+          <img src={`https://i.pravatar.cc/150?img=${usuario.id}`} alt={usuario.nombre} />
+          <p>{usuario.rol}</p>
+          <p>{usuario.descripcion}</p>
+          <p>Lenguajes:</p>
+          <ul>{usuario.lenguajes.map((lenguaje, index) => 
+            (<li key={index}>{lenguaje}</li>))}
+            </ul>
+            </div>))}
     </>
   )
 }
